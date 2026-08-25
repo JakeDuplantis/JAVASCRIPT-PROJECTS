909a88521972593bde7f6db3b6b61ef17ff3f425
Jacob Duplantis <duplantisjacob54@gmail.com>
2026-08-25 11:15:19 -0500

    Added Project1_expressions_alert
    Project1_expressions_alert folder contains index.html and main.js files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var A = "Hello World";                                  //variable
+var Sent1 = "This is the beginning of the string";      //variable
+var Sent2 = " and this is the end of the string";       //variable
+
+window.alert(A);    // window alert method
+
+document.write(Sent1 + Sent2); //document.write method, concatenation
+
+
+
