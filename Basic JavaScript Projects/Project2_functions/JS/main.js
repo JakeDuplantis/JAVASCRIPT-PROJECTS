b12986f7bac7ece93cc838946315bbfa00519547
Jacob Duplantis <duplantisjacob54@gmail.com>
2026-08-25 17:00:15 -0500

    Added Project2_functions folder
    Project2_functions folder contains index.html and main.js files. These files demonstrate onclick event and functions in JavaScript.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+function changeColor(){                                        //Defining a function and naming it
+    var text_blue = document.getElementById("p1");            //Defining variable and giving it an Id in document.getElementById
+    var text_red = document.getElementById("p2");             //Defining variable and giving it an Id in document.getElementById   
+    text_blue.style.color='blue';                             //applies styling for blue text
+    text_red.style.color='red';                               //aplies styling for red text
+    var str = "This is the button text!";							//Defining a variable and giving it a string value                          
+    document.getElementById("Button_Text").innerHTML = str;			//putting the value of the variable into the HTML elementFromPoint with the "Button_Text" id
+}
+
+
+
+	
+
+																	
+	
